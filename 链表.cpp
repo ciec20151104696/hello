bb470a697e98fb04dd1da63afb416526e872c331
ciec20151104696 <1559301693@qq.com>
2016-12-23 16:37:03 +0800

    Add files via upload

---
+++ 链表.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+struct Node
+{
+	char name[40];
+	int age;
+	struct Node *next;
+};
+int main()
+{
+	struct Node *head,*p,*tail;
+	head=new Node;
+	head->next=0;
+	tail=head;
+	scanf("%s",head->name);
+	scanf("%d",&head->age);
+	for(int i=0;i<4;i++)
+	{
+		p=new Node;
+		scanf("%s",p->name);
+		scanf("%d",&p->age);
+		p->next=0;
+		tail->next=p;
+		tail=p;
+	} 
+	p=head;
+	while(p)
+	{
+		printf("%s  %d\n",p->name,p->age);
+		p=p->next;
+	}
+	return 0;
+}
