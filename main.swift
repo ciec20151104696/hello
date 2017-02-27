dff0422e77931fad66fd008b8fa42d63f05ac2bc
ciec20151104696 <1559301693@qq.com>
2017-02-27 11:16:11 +0800

    Add files via upload
    累加swift

---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  frist swift
+//
+//  Created by s20151104696 on 17/2/22.
+//  Copyright © 2017年 s20151104696. All rights reserved.
+//
+
+import Foundation
+var i:Int = 0
+var sum: Int = 0
+for i in 0...100{
+    sum = sum + i
+}
+print(sum)
